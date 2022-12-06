73783a21697938c81060b1fd2f506651591fcb7d
Jeffery <94628699+Mr-Jeffery@users.noreply.github.com>
2022-12-06 19:08:33 +0800

    Update ODEsolver.m

--- ODEsolver.m
+++ ODEsolver.m
@@ -1,25 +1,21 @@
-function output = ODEsolver(alpha,K,a,b,n,r,sigmaP2,k,Slater,Wlater)
-% solver for boundary problem by finite element method
+function output = ODEsolver(alpha,beta,a,b,n,r,sigmaP2,k,Slater,Wlater)
+% solver for boundary problem 
 % u'' + P(x)*u' + Q(x)*u = R(x)     (alpha<x<beta)
 % r1*u'(alpha)  + r2*u(alpha)   = r3
 % r4*u'(beta)   + r5*u(beta)    = r6
 % r can be zero
 
-
 %plug in parametres
-h = (K-alpha)/n;
+h = (beta-alpha)/n;
 M = zeros(n+1);
 D = zeros(n+1,1);
 X = zeros(n+1,1);
-
-
 
 x = alpha;
 %for alpha/x1
 X(1) = alpha;
 M(1,1) = 1;
 D(1) = a;
-
 
 %for x2 to xn
 for i = 2:1:n
@@ -31,12 +27,10 @@ for i = 2:1:n
 end
 D(2:n) = -h^2.*laterValue(Slater,Wlater,X(2:n));
 
-
 %beta/xn+1
-X(n+1) = K;
+X(n+1) = beta;
 M(n+1,n+1) = 1;
 D(n+1) = b;
-
 
 %solve linear equations
 U = M\D;
